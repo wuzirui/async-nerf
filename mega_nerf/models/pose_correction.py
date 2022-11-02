f3c00bab367be5440c685edbda6a84003a08bf11
wuzirui <wuzirui@tztyun.org>
2022-11-02 21:43:03 +0800

    pose correction  settings -- freeze translation

--- mega_nerf/models/pose_correction.py
+++ mega_nerf/models/pose_correction.py
@@ -11,17 +11,19 @@ class PoseCorrection(nn.Module):
     def forward(self, image_indices, rays, depth_mask):
         correction = torch.where(depth_mask == 1, self.correction_dict[image_indices.long()], pp.identity_se3(len(rays)).to(rays.device))
         correction = pp.se3(correction).Exp()
-        rays[:, :3] += correction.translation()
+        rays[:, :3] = (correction.rotation().matrix() @ rays[:, :3, None]).squeeze() #+ correction.translation()
         rays[:, 3:6] = (correction.rotation().matrix() @ rays[:, 3:6, None]).squeeze()
         return rays
     
     def forward_c2ws(self, image_indices, c2ws, depth_mask):
         correction = torch.where(depth_mask == 1, self.correction_dict[image_indices.long()], pp.identity_se3(len(c2ws)).to(c2ws.device))
         correction = pp.se3(correction).Exp()
+        correction[:, :3] = 0
         c2ws_pp = pp.mat2SE3(c2ws.double()).float()     # weird problem of pypose
         ret = correction * c2ws_pp
         return ret
     
     def forward_c2w(self, image_index, c2w):
         correction = pp.se3(self.correction_dict[image_index]).Exp()
+        correction[:3] = 0
         return correction * pp.mat2SE3(c2w.double()).float()
