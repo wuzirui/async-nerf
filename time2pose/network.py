9276895aa51ff21713f426c7101fbfa7dc7cc366
wuzirui <wuzirui@tztyun.org>
2022-11-04 16:28:39 +0800

    update configs

--- time2pose/network.py
+++ time2pose/network.py
@@ -2,10 +2,10 @@ import torch
 from torch import nn
 import math
 
-n_feature = 1240
+n_feature = 300
 def hashcode(num: torch.IntTensor) -> torch.IntTensor:
-    return num + 1
-    # return ((num ** 13).abs() + 1) % n_feature
+    # return num + 2
+    return ((num ** 13).abs() + 1) % n_feature
 
 class TimePoseFunction(nn.Module):
     def __init__(self, hparams):
@@ -14,7 +14,7 @@ class TimePoseFunction(nn.Module):
         self.use_velocity = hparams.velocity_weight > 0
         n_channels = hparams.n_channels
         self.skip = hparams.skip_connections
-        feature_dim = 50
+        feature_dim = 20
         self.input_dir = feature_dim + 1
         self.feature_dict = nn.Parameter(torch.randn(n_feature, feature_dim))
         for i in range(hparams.n_layers):
@@ -36,13 +36,20 @@ class TimePoseFunction(nn.Module):
         self.rotation_output = nn.Sequential(nn.Linear(n_channels, 4))
         self.translation_output = nn.Sequential(nn.Linear(n_channels, 3))
 
+    def _interpolate(self, t):
+        t_mid = t.floor()
+        feat_prev = self.feature_dict[hashcode(t_mid - 1).long()].squeeze(1)
+        feat_mid = self.feature_dict[hashcode(t_mid).long()].squeeze(1)
+        feat_next = self.feature_dict[hashcode(t_mid + 1).long()].squeeze(1)
+        l_prev = (t - t_mid) * (t - t_mid - 1) / 2
+        l_mid = -(t - t_mid + 1) * (t - t_mid -1)
+        l_next = (t - t_mid + 1) * (t - t_mid) / 2
+        return l_prev * feat_prev + l_mid * feat_mid + l_next * feat_next
         
     def forward(self, t, train=False):
         if train and self.use_velocity:
             t = t.requires_grad_(True)
-        t_prev = t.floor()
-        feat_prev, feat_next = self.feature_dict[hashcode(t_prev).long()], self.feature_dict[hashcode(t_prev + 1).long()]
-        feat = (t_prev - t) * feat_prev.squeeze(1) + (t_prev + 1 - t) * feat_next.squeeze(1)
+        feat = self._interpolate(t)
         x = torch.cat([t, feat], -1)
         for i, net in enumerate(self.nets):
             if i in self.skip:
